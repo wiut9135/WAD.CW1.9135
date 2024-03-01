3e9246658ca93ff340736b695fdc228ad9b9c6d6
Elyorbek <wiut00009135@gmail.com>
2024-03-02 01:30:13 +0500

    Creation of the project

---
+++ WAD.CW1.9135/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace WAD.CW1._9135
+{
+	public class WeatherForecast
+	{
+		public DateTime Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string? Summary { get; set; }
+	}
+}
