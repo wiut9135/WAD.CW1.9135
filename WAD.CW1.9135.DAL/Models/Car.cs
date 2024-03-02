2ea40879f14c5e23eb80abffd9ee7228028104b9
Elyorbek <wiut00009135@gmail.com>
2024-03-02 13:27:44 +0500

    Models have been created

---
+++ WAD.CW1.9135.DAL/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace WAD.CW1._9135.DAL.Models
+{
+	public class Car
+	{
+		// Car model properties
+		public int Id { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public decimal PricePerDay { get; set; }
+		public bool IsAvailable { get; set; }
+		public ICollection<Rental> Rentals { get; set; }
+	}
+}
---
+++ WAD.CW1.9135.DAL/Models/Rental.cs
@@ -0,0 +1,15 @@
+﻿namespace WAD.CW1._9135.DAL.Models
+{
+	public class Rental
+	{
+		// Rental model properties
+		public int Id { get; set; }
+		public int CarId { get; set; }
+		public int CustomerId { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public decimal TotalCost { get; set; }
+		public bool IsPaid { get; set; }
+		public Car Car { get; set; }
+	}
+}
