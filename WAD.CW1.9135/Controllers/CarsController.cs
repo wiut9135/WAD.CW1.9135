58793aff7f655cdcf6314a4dd94d2c25d0dd3bb5
Elyorbek <wiut00009135@gmail.com>
2024-03-02 13:31:40 +0500

    Controllers have been developed

---
+++ WAD.CW1.9135/Controllers/CarsController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._9135.DAL.Dtos;
+using WAD.CW1._9135.DAL.Models;
+using WAD.CW1._9135.DAL.Services.Interfaces;
+
+namespace WAD.CW1._9135.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CarsController : ControllerBase
+	{
+		private readonly ICarRepository _carRepository;
+		private readonly IMapper _mapper;
+
+		public CarsController(ICarRepository carRepository, IMapper mapper)
+		{
+			_carRepository = carRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult GetAllCars()
+		{
+			var cars = _carRepository.GetAll();
+			var carDTOs = _mapper.Map<List<CarDto>>(cars);
+			return Ok(carDTOs);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetCarById(int id)
+		{
+			var car = _carRepository.GetById(id);
+			if (car == null)
+			{
+				return NotFound();
+			}
+
+			var carDTO = _mapper.Map<CarDto>(car);
+			return Ok(carDTO);
+		}
+
+		[HttpPost]
+		public IActionResult AddCar([FromBody] CarDto carDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var car = _mapper.Map<Car>(carDto);
+			_carRepository.Add(car);
+
+			var carDtoResult = _mapper.Map<CarDto>(car);
+			return CreatedAtAction(nameof(GetCarById), new { id = carDtoResult.Id }, carDtoResult);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateCar(int id, [FromBody] CarDto carDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var existingCar = _carRepository.GetById(id);
+			if (existingCar == null)
+			{
+				return NotFound();
+			}
+
+			var updatedCar = _mapper.Map(carDto, existingCar);
+			_carRepository.Update(updatedCar);
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteCar(int id)
+		{
+			var car = _carRepository.GetById(id);
+			if (car == null)
+			{
+				return NotFound();
+			}
+
+			_carRepository.Delete(id);
+
+			return NoContent();
+		}
+	}
+}
---
+++ WAD.CW1.9135/Controllers/RentalsController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._9135.DAL.Dtos;
+using WAD.CW1._9135.DAL.Models;
+using WAD.CW1._9135.DAL.Services.Interfaces;
+
+namespace WAD.CW1._9135.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RentalsController : ControllerBase
+	{
+		private readonly IRentalRepository _rentalRepository;
+		private readonly IMapper _mapper;
+
+		public RentalsController(IRentalRepository rentalRepository, IMapper mapper)
+		{
+			_rentalRepository = rentalRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult GetAllRentals()
+		{
+			var rentals = _rentalRepository.GetAll();
+			var rentalDTOs = _mapper.Map<List<RentalDto>>(rentals);
+			return Ok(rentalDTOs);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetRentalById(int id)
+		{
+			var rental = _rentalRepository.GetById(id);
+			if (rental == null)
+			{
+				return NotFound();
+			}
+
+			var rentalDTO = _mapper.Map<RentalDto>(rental);
+			return Ok(rentalDTO);
+		}
+
+		[HttpPost]
+		public IActionResult AddRental([FromBody] RentalDto rentalDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var rental = _mapper.Map<Rental>(rentalDto);
+			_rentalRepository.Add(rental);
+
+			var rentalDtoResult = _mapper.Map<RentalDto>(rental);
+			return CreatedAtAction(nameof(GetRentalById), new { id = rentalDtoResult.Id }, rentalDtoResult);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateRental(int id, [FromBody] RentalDto rentalDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var existingRental = _rentalRepository.GetById(id);
+			if (existingRental == null)
+			{
+				return NotFound();
+			}
+
+			var updatedRental = _mapper.Map(rentalDto, existingRental);
+			_rentalRepository.Update(updatedRental);
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteRental(int id)
+		{
+			var rental = _rentalRepository.GetById(id);
+			if (rental == null)
+			{
+				return NotFound();
+			}
+
+			_rentalRepository.Delete(id);
+
+			return NoContent();
+		}
+	}
+}
