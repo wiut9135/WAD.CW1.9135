99158db975a54c0cae31946c0033c9ac1d749a88
Elyorbek <wiut00009135@gmail.com>
2024-03-02 13:28:05 +0500

    Added application database context

---
+++ WAD.CW1.9135.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.CW1._9135.DAL.Models;
+
+namespace WAD.CW1._9135.DAL.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		// Declaring tables that will be created in migration
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<Rental> Rentals { get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+													: base(options)
+		{ }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// Configuration of relation between models
+			modelBuilder.Entity<Rental>()
+				.HasOne(r => r.Car)
+				.WithMany(c => c.Rentals)
+				.HasForeignKey(r => r.CarId);
+		}
+	}
+}
