031c1df8fc3a163b8cf19558a7ac92947f8fcc10
Elyorbek <wiut00009135@gmail.com>
2024-03-02 13:29:19 +0500

    Interfacse with declared CURD functions

---
+++ WAD.CW1.9135.DAL/Interfaces/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using WAD.CW1._9135.DAL.Models;
+
+namespace WAD.CW1._9135.DAL.Services.Interfaces
+{
+	public interface ICarRepository
+	{
+		IQueryable<Car> GetAll();
+		Car GetById(int id);
+		void Add(Car car);
+		void Update(Car car);
+		void Delete(int id);
+	}
+}
---
+++ WAD.CW1.9135.DAL/Interfaces/IRentalRepository.cs
@@ -0,0 +1,13 @@
+﻿using WAD.CW1._9135.DAL.Models;
+
+namespace WAD.CW1._9135.DAL.Services.Interfaces
+{
+	public interface IRentalRepository
+	{
+		IQueryable<Rental> GetAll();
+		Rental GetById(int id);
+		void Add(Rental rental);
+		void Update(Rental rental);
+		void Delete(int id);
+	}
+}
